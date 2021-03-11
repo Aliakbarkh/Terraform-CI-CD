a51274af964d5e7b8ec1f268e8557d1854f51eb2
Aliakbarkh <ali.vancisero@gmail.com>
2021-03-11 07:35:32 +0700

    Update Files Terraform

--- Terraform/autoscale.tf
+++ Terraform/autoscale.tf
@@ -1,36 +1,36 @@
 data "aws_availability_zones" "available" {}
 
-resource "aws_autoscaling_group" "example" {
+resource "aws_autoscaling_group" "main" {
   name_prefix = "example"
 
   launch_configuration = aws_instance.main.id
   availability_zones   = [data.aws_availability_zones.available.names[0]]
 
-  min_size = 3
+  min_size = 2
   max_size = 5
 
   tags = [
     {
       key                 = "application"
-      value               = "example"
+      value               = "main"
       propagate_at_launch = true
     },
   ]
 }
 
-resource "aws_autoscalingplans_scaling_plan" "example" {
+resource "aws_autoscalingplans_scaling_plan" "main" {
   name = "example-dynamic-cost-optimization"
 
   application_source {
     tag_filter {
       key    = "application"
-      values = ["example"]
+      values = ["main"]
     }
   }
 
   scaling_instruction {
     max_capacity       = 5
-    min_capacity       = 3
+    min_capacity       = 2
     resource_id        = format("autoScalingGroup/%s", aws_autoscaling_group.example.name)
     scalable_dimension = "autoscaling:autoScalingGroup:DesiredCapacity"
     service_namespace  = "autoscaling"
